968c4fd96edd0bcb5178cf79b12b3ae6ea5ab5f3
Luís Magalhães <ei12054@fe.up.pt>
2015-11-20 18:31:42 +0000

    Initial Commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform Objective;
+    public float Speed;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Objective.position, Time.deltaTime * Speed);
+
+        if (Mathf.Approximately(Vector3.Distance(transform.position, Objective.position), 0f))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public Transform Objective;
+    public GameObject EnemyPrefab;
+    public float SpawnTime;
+
+    float _deltaTime;
+
+    void Update()
+    {
+        _deltaTime += Time.deltaTime;
+
+        if (_deltaTime > SpawnTime)
+        {
+            Enemy e = (Instantiate(EnemyPrefab, transform.position, Quaternion.identity) as GameObject).GetComponent<Enemy>();
+            e.Objective = Objective;
+
+            _deltaTime = 0f;
+        }
+    }
+}
